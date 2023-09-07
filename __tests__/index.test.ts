7d48ab97e06b0692b3143a25dc6c7b9867b3bbca
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-09-07 12:04:17 -0700

    fix: add getTerserrc function and fix the tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,9 @@
-import { requireFresh } from "requirefresh"
 import assert from "assert"
-import * as path from "path"
-
-type Terserrc = typeof import("../src/.terserrc")
-function requireTerserrc(): Terserrc {
-  return requireFresh(path.join(path.dirname(__dirname), "src", ".terserrc.js"))
-}
+import { getTerserrc } from "../src/get-terserrc.js"
 
 describe("Terser-Config-Atomic", () => {
   it("production", () => {
-    process.env.NODE_ENV = "production"
-
-    const TerserOptions = requireTerserrc()
+    const TerserOptions = getTerserrc("production", undefined)
 
     expect(typeof TerserOptions).toBe("object")
     assert(typeof TerserOptions.compress === "object")
@@ -28,7 +20,7 @@ describe("Terser-Config-Atomic", () => {
   it("development", () => {
     process.env.NODE_ENV = "development"
 
-    const TerserOptions = requireTerserrc()
+    const TerserOptions = getTerserrc("development", undefined)
 
     expect(typeof TerserOptions).toBe("object")
     expect(TerserOptions.compress).toBe(false)
@@ -38,7 +30,7 @@ describe("Terser-Config-Atomic", () => {
   it("test", () => {
     process.env.NODE_ENV = "test"
 
-    const TerserOptions = requireTerserrc()
+    const TerserOptions = getTerserrc("test", undefined)
 
     expect(typeof TerserOptions).toBe("object")
     assert(typeof TerserOptions.compress === "object")
---
+++ src/get-terserrc.js
@@ -0,0 +1,45 @@
+function getTerserrc(NODE_ENV, BABEL_ENV) {
+  const isDev = NODE_ENV === "development"
+  const isTest = NODE_ENV === "test"
+  const isReadable = isDev || isTest
+
+  const ProductionCompress = {
+    global_defs: {
+      // remove dev and test specific code for production
+      "process.env.NODE_ENV": NODE_ENV || "production",
+      "process.env.BABEL_ENV": BABEL_ENV || NODE_ENV || "production",
+      "@atom.inSpecMode": !isTest ? "() => false" : "() => true",
+      "@atom.inDevMode": !isDev ? "() => false" : "() => true",
+    },
+    ecma: "2018",
+    toplevel: true,
+    hoist_vars: false,
+    hoist_funs: true,
+    pure_getters: true,
+    unsafe: true,
+    unsafe_arrows: true,
+    unsafe_comps: true,
+    unsafe_Function: true,
+    unsafe_math: true,
+    unsafe_symbols: true,
+    unsafe_methods: true,
+    unsafe_proto: true,
+    unsafe_regexp: true,
+    unsafe_undefined: true,
+    passes: 3,
+  }
+
+  const TerserOptions = {
+    // "module": false, // controlled by Parcel
+    compress: isDev ? false : ProductionCompress,
+    parse: {
+      ecma: 2020,
+    },
+    mangle: isReadable ? false : true,
+    format: {
+      beautify: isReadable,
+    },
+  }
+  return TerserOptions
+}
+exports.getTerserrc = getTerserrc
